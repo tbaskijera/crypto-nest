9def2267e3857b2946cfd6ae670d6fe26827669e
Toni Baskijera <tonibaskijera@gmail.com>
2024-06-26 22:01:21 +0200

    Modify eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,8 @@ module.exports = {
   rules: {
     "prettier/prettier": "error",
     "react/self-closing-comp": "error",
+    "react/display-name": "off",
+    "no-redeclare": "off",
+    "@typescript-eslint/no-redeclare": "off",
   },
 };
