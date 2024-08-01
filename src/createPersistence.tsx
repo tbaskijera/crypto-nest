df7f842af03ebdafb84f264044c88fd697c2dba8
Toni Baskijera <tonibaskijera@gmail.com>
2024-08-01 22:03:59 +0200

    Add persistence

---
+++ src/createPersistence.tsx
@@ -0,0 +1,35 @@
+import RNAsyncStorage from "@react-native-async-storage/async-storage";
+
+export function createPersistence(AsyncStorage = RNAsyncStorage) {
+  return {
+    async get(key: string) {
+      const valueString = await AsyncStorage.getItem(key);
+      if (valueString === null) return undefined;
+      try {
+        const value = JSON.parse(valueString);
+        return value;
+      } catch {
+        console.log(
+          "error parsing JSON from persistence for key",
+          key,
+          " value ",
+          valueString,
+        );
+        return undefined;
+      }
+    },
+
+    async set(key: string, value: any) {
+      if (value === undefined) return AsyncStorage.removeItem(key);
+      const valueString = JSON.stringify(value);
+      return AsyncStorage.setItem(key, valueString);
+    },
+
+    async clear(key: string) {
+      return AsyncStorage.removeItem(key);
+    },
+  };
+}
+
+export interface PersistenceStatic
+  extends ReturnType<typeof createPersistence> {}
