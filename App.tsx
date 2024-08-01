4a5b944440b8a67ac04ad4cc1eebc46e41cff708
Toni Baskijera <tonibaskijera@gmail.com>
2024-08-01 21:37:57 +0200

    Setup root app

--- App.tsx
+++ App.tsx
@@ -1,19 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import * as SystemUI from "expo-system-ui";
+import { useEffect, useRef, useState } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ModalProvider } from "./src/components/ModalProvider";
+import { View } from "./src/components/View";
+import { Router } from "./src/navigation/Router";
+import { styleConstants as C } from "./src/styleConstants";
+import { AlertProvider } from "./src/components/alert-provider/AlertProvider";
+import { MSTProvider } from "./src/mobx/MSTProvider";
+import { RootStoreInstance } from "./src/mobx/RootStore";
+import { useMSTFastRefresh } from "./src/mobx/useMSTFastRefresh";
+import { reloadIfStoreInitializedTwice } from "./src/mobx/reloadIfStoreInitializedTwice";
+import { initialize } from "./src/initialize";
 
 export default function App() {
+  const [isAppReady, setIsAppReady] = useState(false);
+  const store = useRef<RootStoreInstance | undefined>(undefined);
+
+  useMSTFastRefresh(store);
+
+  useEffect(() => {
+    reloadIfStoreInitializedTwice(store.current);
+
+    const init = async () => {
+      const context = await initialize();
+      store.current = context.store;
+      await SystemUI.setBackgroundColorAsync(C.colorDark);
+      setIsAppReady(true);
+    };
+    init();
+  }, []);
+
+  if (!isAppReady) {
+    return <View flex colorDark />;
+  }
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-    </View>
+    <MSTProvider store={store.current}>
+      <SafeAreaProvider>
+        <ModalProvider>
+          <AlertProvider>
+            <StatusBar style="light" />
+            <Router />
+          </AlertProvider>
+        </ModalProvider>
+      </SafeAreaProvider>
+    </MSTProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
