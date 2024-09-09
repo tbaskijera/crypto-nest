5a535e50119f4f38bec8c7cfbf7705cebca2bac3
Toni Baskijera <tonibaskijera@gmail.com>
2024-09-09 20:22:32 +0200

    Custom handling of inactive state on android

--- src/features/user-activity-provider/UserActivityProvider.tsx
+++ src/features/user-activity-provider/UserActivityProvider.tsx
@@ -1,8 +1,10 @@
 import { useNavigation } from "@react-navigation/native";
 import { observer } from "mobx-react-lite";
 import { ReactNode, useEffect, useRef } from "react";
-import { AppState } from "react-native";
+import { AppState, NativeEventEmitter, Platform } from "react-native";
 import { useStore } from "../../mobx/utils/useStore";
+
+const NativeEvent = Platform.OS === "android" ? new NativeEventEmitter() : null;
 
 export const UserActivityProvider = observer(function UserActivityProvider({
   children,
@@ -17,9 +19,12 @@ export const UserActivityProvider = observer(function UserActivityProvider({
 
   useEffect(() => {
     const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if (store.uiStore.isRequestingPermissions) return;
       if (!hasActiveWallet) return;
+      console.warn(AppState.currentState);
 
       if (nextAppState === "inactive") {
+        if (Platform.OS === "android") return;
         navigation.navigate("PrivacyScreen");
       } else {
         navigation.navigate("LockScreen");
@@ -29,7 +34,22 @@ export const UserActivityProvider = observer(function UserActivityProvider({
     });
 
     return () => subscription.remove();
-  }, [hasActiveWallet, navigation]);
+  }, [hasActiveWallet, navigation, store.uiStore.isRequestingPermissions]);
+
+  useEffect(() => {
+    if (store.uiStore.isRequestingPermissions) return;
+    if (Platform.OS === "ios") return;
+    if (NativeEvent === null) return;
+    const handleActiveState = NativeEvent.addListener(
+      "ActivityStateChange",
+      (e) => {
+        if (e.event === "inactive") navigation.navigate("PrivacyScreen");
+      },
+    );
+    return () => {
+      handleActiveState.remove();
+    };
+  }, [navigation, store.uiStore.isRequestingPermissions]);
 
   return children;
 });
