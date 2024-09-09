47092ebe4de39fad7f31a13a35b554efc6a7579e
Toni Baskijera <tonibaskijera@gmail.com>
2024-09-09 20:21:55 +0200

    Refactor account entity

--- src/mobx/entities/account/Account.tsx
+++ src/mobx/entities/account/Account.tsx
@@ -5,19 +5,22 @@ export interface AccountInstance extends Instance<typeof Account> {}
 export interface AccountSnapshotIn extends SnapshotIn<typeof Account> {}
 export interface AccountSnapshotOut extends SnapshotOut<typeof Account> {}
 
-export const Account = types.model("Account", {
-  id: types.identifier,
-  index: types.number,
-  title: types.optional(types.string, "Default"),
-  default: types.boolean,
-  derivationPath: "bip44Change",
-  tokens: types.model("Tokens", {
-    master: types.model({
-      publicKey: types.string,
-      mintKey: types.string,
+export const Account = types
+  .model("Account", {
+    id: types.identifier,
+    index: types.number,
+    title: types.optional(types.string, "Default"),
+    default: types.boolean,
+    derivationPath: "bip44Change",
+    tokens: types.model("Tokens", {
+      master: types.model({
+        publicKey: types.string,
+        mintKey: types.string,
+      }),
     }),
-    // devnet: types.array(types.string),
-    // testnet: types.array(types.string),
-    // mainnet: types.array(types.string),
-  }),
-});
+  })
+  .actions((self) => ({
+    changeAccountName(newName: string) {
+      self.title = newName;
+    },
+  }));
